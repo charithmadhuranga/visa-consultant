d573cb5b70e521b2c1a83aff6e25ec4da3de6a2c
Charith wijesundara <nikhuge@gmail.com>
2024-06-22 20:32:31 +0530

    done

--- js/slitslider-custom.js
+++ js/slitslider-custom.js
@@ -1,3 +1,38 @@
+// $(function () {
+//   var Page = (function () {
+//     var timer = null;
+
+//     var $nav = $("#nav-dots > span"),
+//       slitslider = $("#slider").slitslider({
+//         onBeforeChange: function (slide, pos) {
+//           $nav.removeClass("nav-dot-current");
+//           $nav.eq(pos).addClass("nav-dot-current");
+//         },
+//       }),
+//       init = function () {
+//         initEvents();
+//       },
+//       initEvents = function () {
+//         $nav.each(function (i) {
+//           $(this).on("click", function (event) {
+//             var $dot = $(this);
+
+//             if (!slitslider.isActive()) {
+//               $nav.removeClass("nav-dot-current");
+//               $dot.addClass("nav-dot-current");
+//             }
+
+//             slitslider.jump(i + 1);
+//             return false;
+//           });
+//         });
+//       };
+
+//     return { init: init };
+//   })();
+
+//   Page.init();
+// });
 $(function () {
   var Page = (function () {
     var timer = null;
@@ -11,6 +46,7 @@ $(function () {
       }),
       init = function () {
         initEvents();
+        startAutoSlide();
       },
       initEvents = function () {
         $nav.each(function (i) {
@@ -26,6 +62,11 @@ $(function () {
             return false;
           });
         });
+      },
+      startAutoSlide = function () {
+        timer = setInterval(function () {
+          slitslider.next();
+        }, 5000); // Change slide every 10 seconds
       };
 
     return { init: init };
